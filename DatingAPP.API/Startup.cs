2398bea8a692b0d286bcb03b7a8fbe646e208da9
mohamed magdy <m.magdy05@gmail.com>
2020-06-13 12:06:32 +0300

    hello

--- DatingAPP.API/Startup.cs
+++ DatingAPP.API/Startup.cs
@@ -24,7 +24,7 @@ namespace DatingAPP.API
 
         public IConfiguration Configuration { get; }
            
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. magdy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
